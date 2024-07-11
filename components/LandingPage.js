42464c27aa8f9760da9693202c9d07a66f62f17a
Bratee Podder <bpodder3@gatech.edu>
2024-07-12 03:35:45 +0400

    tried my best + still creating rest of landing page lol

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -6,15 +6,31 @@ const logo = "/assets/logoLight.png";
 const LandingPage = () => {
   return (
     <div className={styles.landingPage}>
-      <img src={headerImage} alt="Cafe Header" className={styles.headerImage} />
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="Logo" className={styles.rotatingLogo} />
-      </div>
-      <section className={styles.content}>
-        <h1>Welcome to Sip & Play Café</h1>
-        <p>Forget and savor that sip.</p>
-      </section>
+        <div className="block">
+            <img src={headerImage} alt="Cafe Header" className={styles.headerImage} />
+            <div className={styles.logoContainer}>
+            <img src={logo} alt="Logo" className={styles.rotatingLogo} />
+        </div>
+        </div>
+        <div>
+            <div className={styles.bigTextContainer}>
+                <div className={styles.textContainer}>
+                    <p className={styles.greenTitle} >Who even</p>
+                    <p className={styles.textLeft}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className={styles.separator}></div>
+                <div className={styles.textContainer}>
+                    <p className={styles.greenTitle}>are we?</p>
+                    <p className={styles.textRight}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
+    
   );
 };
 

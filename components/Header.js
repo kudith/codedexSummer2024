9d3e0f41cf0fd9c8165e2f327de21008287c66a1
Bratee Podder <bpodder3@gatech.edu>
2024-07-12 12:37:58 +0400

    2nd section complete, just need image

--- components/Header.js
+++ components/Header.js
@@ -2,14 +2,14 @@
 
 import React, { useState, useEffect } from 'react';
 import styles from './header.module.css';
+import RightNav from './RightNav.js';
 const logo = "/assets/logo.png";
 const header = "/assets/header-bg.jpg";
 
 const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -21,47 +21,14 @@ const Header = () => {
           </div>
 
           {/* Right section with navigation tabs and mode switch button */}
-          <div className={styles.right}>
-              <nav className={styles.navLinks}>
-                  <ul>
-                      <li><a href="/">home</a></li>
-                      <li><a href="/menu">menu</a></li>
-                      <li><a href="/events">events</a></li>
-                  </ul>
-              </nav>
-              {/* Toggle switch */}
-                <label className={styles.switch}>
-                    <input type="checkbox" checked={isDarkMode} onChange={toggleDarkMode} />
-                    <span className={`${styles.slider} ${styles.round}`}></span>
-                </label>
-          </div>
+            <div className={styles.hamburger} onClick={toggleMenu}>
+              <div className={styles.bar}></div>
+              <div className={styles.bar}></div>
+              <div className={styles.bar}></div>
+            </div>
+            <RightNav />
       </header>
   );
 };
 
 export default Header;
-
-
-// import Image from 'next/image'
-
-// export default function Header() {
-//   return (
-//     <header className="relative h-screen">
-//       <div className="relative h-full w-full">
-//         <Image
-//           src="/assets/header-bg.jpg"
-//           alt="Cafe background"
-//           fill
-//           quality={100}
-//           className="absolute inset-0 z-0 object-cover"
-//         />
-//         <div className="absolute inset-0 bg-black opacity-50"></div>
-//         <div className="relative z-10 flex items-center justify-center h-full text-center text-eggshell">
-//           <div>
-//             <h1 className="text-5xl font-bold">Welcome to Sip & Play Cafe</h1>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   )
-// }
---
+++ components/RightNav.js
@@ -0,0 +1,33 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import styles from "./RightNav.module.css";
+
+const RightNav = () => {
+    // const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+      };
+    
+
+    return (
+        <div className={styles.right}>
+            <nav className={styles.navLinks}>
+              <ul>
+                <li><a href="/">home</a></li>
+                <li><a href="/menu">menu</a></li>
+                <li><a href="/events">events</a></li>
+                {/* Toggle switch
+                <label className={styles.switch}>
+                    <input type="checkbox" checked={isDarkMode} onChange={toggleDarkMode} />
+                    <span className={`${styles.slider} ${styles.round}`}></span>
+                </label> */}
+              </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default RightNav;

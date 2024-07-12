dda470afb4c06c538549669980e81fe0f4029dab
Bratee Podder <bpodder3@gatech.edu>
2024-07-12 15:31:16 +0400

    pushed changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'next-themes'
 import './globals.css'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export const metadata = {
   title: 'Sip & Play Cafe',
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class">
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>

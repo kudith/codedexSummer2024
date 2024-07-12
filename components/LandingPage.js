0e5980a6f76edb0de5c50b48bc5784b8decff777
kudith <ginanjar.aditya15@gmail.com>
2024-07-12 22:37:03 +0700

    chore: Update landing page link to Sip&Play website

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -42,7 +42,7 @@ const LandingPage = () => {
           <p className={styles.section2Header}>so, how do we work?</p>
           <p className={styles.section2Body}>
             Come on in with your friends and family and play board games from our collection of over 500+ games! Just $10 a person for 3 hours of gameplay. ($12 a person Friday-Sunday and Holidays) Play while enjoying bubble tea, coffee, beer sandwiches or salads and have a great time! If you want to guarantee a table, you can make a reservation which is $15 a person for 3 hours of gameplay. <br />
-            <a href="https://www.exploretock.com/sipnplay/">https://www.exploretock.com/sipnplay/</a>
+            <a className="text-2xl font-bold font-inknut underline" href="https://www.exploretock.com/sipnplay/">Sip&Play</a>
           </p>
         </div>
       </div>

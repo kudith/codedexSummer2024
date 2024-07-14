45acbf48966a2698a26351a5db3785d194b1ff06
kudith <ginanjar.aditya15@gmail.com>
2024-07-14 07:31:39 +0700

    fix: modify font in footer

--- components/Footer.js
+++ components/Footer.js
@@ -19,13 +19,13 @@ const Footer = () => {
           <div className="flex-1 space-y-6">
             {/* Created By */}
             <div>
-              <h3 className="text-lg font-bold text-light-green font-hind mb-2">Created By</h3>
+              <h1 className="text-lg font-bold text-light-green mb-2">Created By</h1>
               <p className="font-hind text-sm">This website was created for Codédex Summer Hackathon 2024 by Bratee, Aditya, and Ritwik.</p>
             </div>
 
             {/* Contact Us */}
             <div>
-              <h3 className="text-lg font-bold text-light-green font-hind mb-2">Contact Us</h3>
+              <h1 className="text-lg font-bold text-light-green mb-2">Contact Us</h1>
               <ul className="list-none space-y-1 font-hind text-sm">
                 <li>sipnplaynyc@gmail.com</li>
                 <li>718-971-1684</li>
@@ -49,7 +49,7 @@ const Footer = () => {
 
           {/* Location */}
           <div className="flex-1">
-            <h3 className="text-lg font-bold text-light-green font-hind mb-2">Location</h3>
+            <h1 className="text-lg font-bold text-light-green mb-2">Location</h1>
             <p className="font-hind text-sm mb-4">471 5th Ave Brooklyn, NY 11215</p>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3026.265213180678!2d-73.9869174!3d40.668124999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25afce910cb65%3A0x51750a9972fb4c0a!2s471%205th%20Ave%2C%20Brooklyn%2C%20NY%2011215%2C%20USA!5e0!3m2!1sen!2sae!4v1720780877931!5m2!1sen!2sae"

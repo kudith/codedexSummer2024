335032cd37306424b1140b9416c7a2ca3722d0d8
kudith <ginanjar.aditya15@gmail.com>
2024-07-26 19:33:21 +0700

    modify CafeHero.js: opacity logo and responsive

--- components/CafeHero.js
+++ components/CafeHero.js
@@ -8,10 +8,10 @@ const logo = "/assets/logo.png"
 const CafeHero = () => {
   return (
     <div className="relative z-10 bg-sea-blue">
-        <div className="relative h-[70vh]">
+        <div className="relative h-[75vh]">
         <video src={headerVideo} className="w-full h-full object-cover" autoPlay loop muted />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/5 h-1/5 flex justify-center items-center">
-                <img src={logo} alt="Logo" className="rotating bg-light-green rounded-full border-4 border-teal" />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-1/5 md:h-1/5 flex justify-center items-center w-2/5 h-2/5">
+                <img src={logo} alt="Logo" className="rotating md:w-full bg-light-green/80 rounded-full border-4 border-teal/80" />
             </div>
         </div>
     </div>

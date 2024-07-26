9715a04e5e5a8288a56cb475f201538c1c38e202
kudith <ginanjar.aditya15@gmail.com>
2024-07-26 22:09:21 +0700

    refactor: reorganized the components folder structure, modified mobile menu, and installed framer-motion

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import logo from "@/public/assets/logo.png";
 import React from "react";
 
-export default function () {
+export default function Logo() {
     return (
         <div className="flex items-center space-x-4">
             <div className="relative w-20 h-20">

5d5c0a6cd9e2495556e72651c1233fe195f055e9
Bratee Podder <bpodder3@gatech.edu>
2024-07-28 18:14:16 +0400

    Update CafeHero.js

--- components/Section/CafeHero.js
+++ components/Section/CafeHero.js
@@ -11,7 +11,7 @@ const CafeHero = () => {
         <div className="relative h-[75vh]">
         <video src={headerVideo} className="w-full h-full object-cover" autoPlay loop muted />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-1/5 md:h-1/5 flex justify-center items-center w-2/5 h-2/5">
-                <img src={logo} alt="Logo" className="rotating md:w-full bg-light-green/80 rounded-full border-4 border-teal/80" />
+                <img src={logo} alt="Logo" className="rotating md:w-full bg-light-green rounded-full border-4 border-teal/80" />
             </div>
         </div>
     </div>
